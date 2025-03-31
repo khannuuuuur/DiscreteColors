25d9b400b192518ba7dda8d1832020a678d8044d
khannuuuuur <hsu.conner@gmail.com>
2025-03-30 23:58:46 -0700

    poop

--- QuantumColors.py
+++ QuantumColors.py
@@ -15,7 +15,9 @@ def save_image(imageArray, name):
     count = 1
     while path.exists(name[:name.index('.')] + str(count) + name[name.index('.'):]):
         count += 1
-    Image.fromarray(imageArray).save(name[:name.index('.')] + str(count) + name[name.index('.'):])
+    name = name[:name.index('.')] + str(count) + name[name.index('.'):]
+    Image.fromarray(imageArray).save(name)
+    return name
 
 #========================================================
 #                       K-means
@@ -114,6 +116,15 @@ def locally_scramble_labels(labels, scale=0.5):
                  new_labels[xn,yn] = label
     return new_labels
 
+def get_labels(image, centroids):
+
+    colors = image.reshape([image.shape[0]*image.shape[1], 3])
+    distances = np.linalg.norm(colors[:, np.newaxis] - centroids, axis=2)
+    labels = np.argmin(distances, axis=1)
+    labels = labels.reshape([image.shape[0], image.shape[1]])
+    return labels
+
+
 def map_palette(image, labels, centroids, new_palette, mapping):
     neighbors = np.array([[1,1]])
 
@@ -143,43 +154,84 @@ def map_palette(image, labels, centroids, new_palette, mapping):
             image[x,y] = image[x,y]*c + (1-c)*color
     return np.clip(image, 0, 255).astype('uint8')
 
-def main():
-    image = open_image("matterhorn.jpg")
-    print("Image loaded.")
-    NUM_COLORS = 5
+def swap(arr, i, j):
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
 
-    #
+def main():
+    image = open_image("zermatt2.jpg")
+    print("Image loaded.")
+    NUM_COLORS = 10
+
+    """
     colors = image.reshape([image.shape[0]*image.shape[1], 3])
     centroids = init_means(image, NUM_COLORS, size=2)
     centroids, labels = k_means(colors, centroids)
     centroids = centroids.astype('uint8')
     labels = labels.reshape([image.shape[0], image.shape[1]])
+    print(centroids)
+    """
+
+    centroids = np.array([[161, 183, 219],
+                         [ 36,  46,  50],
+                         [119, 129, 142],
+                         [137, 149, 165],
+                         [180, 179, 179],
+                         [ 94, 111, 133],
+                         [ 67,  85, 102],
+                         [224, 232, 241],
+                         [142, 169, 213],
+                         [196, 206, 220]])
+    labels = get_labels(image, centroids)
+    #labels = locally_scramble_labels(labels)
 
 
+    """
     aro = np.array([[61, 165, 66],
                     [167, 211, 121],
                     [255,255,255],
                     [169, 169, 169],
                     [0,0,0]])
     """
+
+    aro = np.array([[226.25237701, 228.60571271, 229.18341421], # 0, gray 230
+                    [174.32817404, 211.0946505,  140.04194009], # 1, light green
+                    [125.75347863, 128.12687902, 130.67236658], # 2, gray 130
+                    [  5.68240261,   6.18956694,   6.88546664], # 3, black
+                    [113.01251307, 159.43222386,  92.19040063], # 4, medium green
+                    [ 59.49601538, 131.74868742,  57.21334146], # 5, dark green
+                    [190.17555625, 193.94198378, 196.60859846], # 6, gray 193
+                    [157.34862714, 159.56541982, 162.19371269], # 7, gray 160
+                    [144.0325094,  182.08527673, 110.73680817], # 8, light-medium green
+                    [ 63.16066066,  64.18468468,  65.7972973 ]]) # 9, gray 65
+    """
+
     aro_colors = open_image("aro2.jpg")
     aro = init_means(aro_colors, NUM_COLORS, size=2)
     aro_colors = aro_colors.reshape([aro_colors.shape[0]*aro_colors.shape[1], 3])
     aro, _ = k_means(aro_colors, aro)
+
+    print(aro)
     """
 
     #mapping = assign_new_pallete(centroids, aro)
-    mapping = []
+    # index: which color on the new palette, value: which color in the old palette
+    #mapping = [7, 4, 2, 1, 3, 5, 9, 8, 0, 6]
+    #          0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    mapping = [7, 4, 2, 1, 3, 5, 9, 8, 0, 6]
+    swap(mapping, 8, 7)
 
-    #labels = locally_scramble_labels(labels)
+    print(mapping)
+    #mapping = np.arange(NUM_COLORS)
+    #np.random.shuffle(mapping)
+    #mapping = [x for x in mapping]
     image = map_palette(image, labels, centroids, aro, mapping)
 
 
 
-    print("Quantized colors.")
 
-    save_image(image, "out.png")
-    print("Done.")
+    print("Saved image as", save_image(image, "out.png"))
 
 if __name__ == '__main__':
     main()
